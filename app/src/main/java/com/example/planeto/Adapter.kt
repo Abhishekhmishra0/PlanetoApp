3b033623f5b85485133736f431e7e15ef8869bdc
abhishekhmishra <mabhishekh51@gmail.com>
2025-01-10 12:36:27 +0530

    First Comit

---
+++ app/src/main/java/com/example/planeto/Adapter.kt
@@ -0,0 +1,75 @@
+package com.example.planeto
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class Adapter(var planet: List<PlanetData>): RecyclerView.Adapter<Adapter.myViewHolder>(){
+    class myViewHolder(view:View): RecyclerView.ViewHolder(view){
+        var title = view.findViewById<TextView>(R.id.title)
+        var planetImg = view.findViewById<ImageView>(R.id.planet_img)
+        var galaxy = view.findViewById<TextView>(R.id.galaxy)
+        var distance = view.findViewById<TextView>(R.id.distance)
+        var gravity = view.findViewById<TextView>(R.id.gravity)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): myViewHolder {
+        var itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.view,parent,false)
+        return myViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return planet.size
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: myViewHolder, position: Int) {
+        var dummyImage:Int?=null
+        holder.title.text = planet[position].title
+        holder.galaxy.text = planet[position].galaxy
+        holder.distance.text = planet[position].distance + "m km"
+        holder.gravity.text = planet[position].gravity+ "m/ss"
+
+        when(planet[position].title!!.toLowerCase())
+        {
+            "mars"->{
+                dummyImage = R.drawable.mars
+            }
+            "neptune"->{
+                dummyImage = R.drawable.neptune
+            }
+            "jupiter"->{
+                dummyImage = R.drawable.jupiter
+            }
+            "moon"->{
+                dummyImage = R.drawable.moon
+            }
+            "sun"->{
+                dummyImage = R.drawable.sun
+            }
+            "uranus"->{
+                dummyImage = R.drawable.uranus
+            }
+            "mercury"->{
+                dummyImage = R.drawable.mercury
+            }
+            "saturn"->{
+                dummyImage = R.drawable.saturn
+            }
+            "earth" -> {
+                dummyImage = R.drawable.earth
+            }
+            "venus"->{
+                dummyImage = R.drawable.venus
+            }
+
+        }
+        holder.planetImg.setImageResource(dummyImage!!)
+    }
+}
+
---
+++ app/src/main/java/com/example/planeto/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.planeto
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val myRecycler = findViewById<RecyclerView>(R.id.my_recycler)
+        myRecycler.adapter = Adapter(setData.SetPlanets())
+        myRecycler.layoutManager = LinearLayoutManager(this)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
